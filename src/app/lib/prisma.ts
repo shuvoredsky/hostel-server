46ece1b2236af84ce6c97fb68a92f7a95b4906ef
shuvo chakrabrati <kumarshuvo265@gmail.com>
2026-04-06 01:00:12 +0600

    fix: postinstall generate and migrate

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '../../generated';
+// import { PrismaClient } from '../../generated';
+import { PrismaClient } from '@prisma/client';
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!,
